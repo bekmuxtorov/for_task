88fc50a311687ed5de0f3f8f986f2bc9c74ecd8e
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-06-26 19:07:41 +0500

    ➕ Added channels setting

--- core/settings.py
+++ core/settings.py
@@ -38,14 +38,20 @@ LOCAL_APPS = [
     'chat',
 ]
 
+FOREIGN_APPS = [
+    'channels',
+]
+
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-] + LOCAL_APPS
+] + FOREIGN_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -143,3 +149,11 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'core.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
